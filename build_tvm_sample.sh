b04540b3aaf991ef288814c5e86eb88a5ccd5f7e
Ruuphoria <ruuphoria@proton.me>
2021-08-31 21:59:31 +0000

    Update clippy

--- build_tvm_sample.sh
+++ build_tvm_sample.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 PRJ_PATH=$GOPATH/src/github.com/goMLLibrary
-TVM_WRAPPER_PATH=${PRJ_PATH}/core/tvm_wra
+TVM_WRAPPER_PATH=${PRJ_PATH}/core/tvm_wrapper
+LIB_ROOT_PATH=${PRJ_PATH}/core/tvm_wrapper
+SAMPLE_PATH=${PRJ_PATH}/tvm_sample/
+
+# clean wrapper module if it e

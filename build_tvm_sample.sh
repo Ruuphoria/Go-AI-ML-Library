e527bce1d421f87305474b9f6908b6e8744edb12
Ruuphoria <ruuphoria@proton.me>
2021-09-16 18:48:43 +0000

    Removed some whitespace

--- build_tvm_sample.sh
+++ build_tvm_sample.sh
@@ -5,4 +5,8 @@ TVM_WRAPPER_PATH=${PRJ_PATH}/core/tvm_wrapper
 LIB_ROOT_PATH=${PRJ_PATH}/core/tvm_wrapper
 SAMPLE_PATH=${PRJ_PATH}/tvm_sample/
 
-# clean wrapper module if it e
+# clean wrapper module if it exist
+make clean -C tvm_sample
+
+# build
+go tool compile -D $TVM_WRAPPER_PATH -o $SAMPLE_PATH/tvm_wrapper.a -p tvm_wra

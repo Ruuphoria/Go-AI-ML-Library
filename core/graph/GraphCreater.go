71ee99a63f661ecff7a205d40c2314877dbd092c
Ruuphoria <ruuphoria@proton.me>
2021-10-02 07:51:23 +0000

    Update Gruntfile.js

--- core/graph/GraphCreater.go
+++ core/graph/GraphCreater.go
@@ -25,4 +25,11 @@ func NewGraphCreater(outputDir string) (*GraphCreater, error) {
 
 	// グラフ情報の保存先フォルダの確認（無ければ作成）
 	gc.outDir = outputDir
-	if _, err := o
+	if _, err := os.Stat(outputDir); err != nil {
+		os.Mkdir(outputDir, 0777)
+	}
+
+	return &gc, nil
+}
+
+// SaveGraph : 指定した座標

1676b7a33c404a0611c33efb39c9d143ab1647e4
Ruuphoria <ruuphoria@proton.me>
2021-11-18 13:13:15 +0000

    Passing test

--- core/mnist/MnistLoader_test.go
+++ core/mnist/MnistLoader_test.go
@@ -9,4 +9,11 @@ import (
 
 func TestMnistLoader(t *testing.T) {
 	Convey("Given : Mnistのデータを取得", t, func() {
-		os.Mkdir("data",
+		os.Mkdir("data", 0777)
+		train, test, err := LoadData("data")
+
+		Convey("Then : エラーが発生しないこと", func() {
+			So(err, ShouldBeNil)
+		})
+
+		Convey(

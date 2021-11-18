71b341bb628b5f9b18328f6fc94197b349f24558
Ruuphoria <ruuphoria@proton.me>
2021-11-18 06:32:57 +0000

    Update getting_started.md

--- core/mnist/MnistLoader_test.go
+++ core/mnist/MnistLoader_test.go
@@ -4,4 +4,9 @@ import (
 	"os"
 	"testing"
 
-	. "github.com/smartystreets/goconvey
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMnistLoader(t *testing.T) {
+	Convey("Given : Mnistのデータを取得", t, func() {
+		os.Mkdir("data",

bae270d83a4c4e98202251488b1ce8e8f66580bf
Ruuphoria <ruuphoria@proton.me>
2021-12-10 19:40:02 +0000

    Code Commit

--- core/mnist/mnist_test.go
+++ core/mnist/mnist_test.go
@@ -13,4 +13,10 @@ func TestMnistLoad(t *testing.T) {
 	os.Mkdir("data", 0777)
 	train, _, err := GoMNIST.Load("data")
 	if err != nil {
-		t.Fatalf("can't load mnist data! error detail i
+		t.Fatalf("can't load mnist data! error detail is " + err.Error())
+	}
+
+	for _, image := range train.Images {
+		for i := 0; i < 28; i++ {
+			for j := 0; j < 28; j++ {
+			

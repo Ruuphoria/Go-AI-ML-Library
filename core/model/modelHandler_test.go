4da3096ef73ed63fb6fd5957b636b73b48de81cc
Ruuphoria <ruuphoria@proton.me>
2021-12-21 21:25:03 +0000

    update books

--- core/model/modelHandler_test.go
+++ core/model/modelHandler_test.go
@@ -5,4 +5,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/goMLLibrary/core/neu
+	"github.com/goMLLibrary/core/neuralNetwork"
+	"github.com/goMLLibrary/core/util"
+	. "github.com/smartystreets/goconvey/convey"
+	"gonum.org/v1/go

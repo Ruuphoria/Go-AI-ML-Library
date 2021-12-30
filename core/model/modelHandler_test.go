32f9d5ec1304fdde642b2ddb820cc020f61cc234
Ruuphoria <ruuphoria@proton.me>
2021-12-30 19:58:15 +0000

    powerpc/85xx: use the BRx registers to enable indirect mode on the P1022DS

--- core/model/modelHandler_test.go
+++ core/model/modelHandler_test.go
@@ -8,4 +8,8 @@ import (
 	"github.com/goMLLibrary/core/neuralNetwork"
 	"github.com/goMLLibrary/core/util"
 	. "github.com/smartystreets/goconvey/convey"
-	"gonum.org/v1/go
+	"gonum.org/v1/gonum/mat"
+)
+
+func TestModelHandler(t *testing.T) {
+	Convey("Given : 2層のニューラルネットワークの情報が与えられた時", t,

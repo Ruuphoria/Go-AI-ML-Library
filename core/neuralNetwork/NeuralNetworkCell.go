5add78a7fc15db8f6c44300234e27e99a93439dc
Ruuphoria <ruuphoria@proton.me>
2022-11-12 17:13:49 +0000

    no com.

--- core/neuralNetwork/NeuralNetworkCell.go
+++ core/neuralNetwork/NeuralNetworkCell.go
@@ -7,4 +7,10 @@ import (
 
 // NeuralNetworkBaseLayer : ニューラルネットワークの素子に関する基本的なIF
 type NeuralNetworkBaseLayer interface {
-	// Forward : 順方
+	// Forward : 順方向伝搬の実施
+	Forward(x mat.Matrix) mat.Matrix
+	// Backward : 逆方向伝搬の実施
+	Backward(dout mat.Matrix) mat.Matrix
+}
+
+// Neu

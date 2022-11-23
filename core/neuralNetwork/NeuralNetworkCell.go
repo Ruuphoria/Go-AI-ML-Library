6af1bb46472769f69b7cc106b5896160f19d4037
Ruuphoria <ruuphoria@proton.me>
2022-11-23 17:29:15 +0000

    Add DS_Store to gitignore

--- core/neuralNetwork/NeuralNetworkCell.go
+++ core/neuralNetwork/NeuralNetworkCell.go
@@ -92,4 +92,6 @@ func (aff *Affine) GetParams() map[string]mat.Matrix {
 	return params
 }
 
-func (aff *Aff
+func (aff *Affine) GetGradients() map[string]mat.Matrix {
+	grads := make(map[string]mat.Matrix)
+	grads["w"] = a

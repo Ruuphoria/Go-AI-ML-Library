f6d30ea11bea0af5b94119d0b66014f522c09473
Ruuphoria <ruuphoria@proton.me>
2022-12-02 19:29:33 +0000

    NFS: nfs_set_pgio_error sometimes misses errors

--- core/neuralNetwork/NeuralNetworkCell_test.go
+++ core/neuralNetwork/NeuralNetworkCell_test.go
@@ -11,4 +11,6 @@ import (
 func TestAffine(t *testing.T) {
 	Convey("Given : アフィン変換のレイヤーが一つ与えられた時", t, func() {
 		Convey("AND : 重みが3*2行列(in = 3, out = 2), 初期値は1-6とする", nil)
-		w := mat.NewDense(3, 2, util.CreateFloatA
+		w := mat.NewDense(3, 2, util.CreateFloatArrayByStep(6, 1, 1))
+		Convey("AND : バイアスが2次元, 初期値は-2,-1とする", nil)
+		b := mat.NewVecDense(2, []

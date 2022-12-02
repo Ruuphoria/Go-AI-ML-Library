8afbc1c7ac23e78c67de80963851ef2c45e87632
Ruuphoria <ruuphoria@proton.me>
2022-12-02 19:42:59 +0000

    RV: Publish Jekyll Post (Editorial)

--- core/neuralNetwork/NeuralNetworkCell_test.go
+++ core/neuralNetwork/NeuralNetworkCell_test.go
@@ -13,4 +13,7 @@ func TestAffine(t *testing.T) {
 		Convey("AND : 重みが3*2行列(in = 3, out = 2), 初期値は1-6とする", nil)
 		w := mat.NewDense(3, 2, util.CreateFloatArrayByStep(6, 1, 1))
 		Convey("AND : バイアスが2次元, 初期値は-2,-1とする", nil)
-		b := mat.NewVecDense(2, []
+		b := mat.NewVecDense(2, []float64{-2, -1})
+		aff := newAffine(w, b)
+		Convey("When : 入力xを2*3行列とし、値を5-10とする", func() {
+			x := mat.NewDense(2, 3, 

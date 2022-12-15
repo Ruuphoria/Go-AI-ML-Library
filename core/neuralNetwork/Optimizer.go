c27e8840a1bd3c91a3c48252ad0268131c3ff935
Ruuphoria <ruuphoria@proton.me>
2022-12-15 08:31:43 +0000

    6.2

--- core/neuralNetwork/Optimizer.go
+++ core/neuralNetwork/Optimizer.go
@@ -5,4 +5,6 @@ import (
 )
 
 // Optimizer : パラメーターと勾配情報からパラメーターの最適化を行うIF
-type Optimizer interfa
+type Optimizer interface {
+	// Update : パラメーターを勾配情報を元に最適化(更新)する
+	Update(params map[string]mat.Matrix, grads map[string]m

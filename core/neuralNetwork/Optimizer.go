2e2bffea46356a680b42e12b214ce0eb22e029a2
Ruuphoria <ruuphoria@proton.me>
2022-12-17 18:45:13 +0000

    last minute

--- core/neuralNetwork/Optimizer.go
+++ core/neuralNetwork/Optimizer.go
@@ -43,4 +43,7 @@ func WithSGDLearningRate(lr float64) SGDOption {
 	}
 }
 
-func (sgd *SGD) Update(params map[string]mat.Matrix, g
+func (sgd *SGD) Update(params map[string]mat.Matrix, grads map[string]mat.Matrix) {
+	for key, _ := range params {
+		//r, c := params[key].Dims()
+		den

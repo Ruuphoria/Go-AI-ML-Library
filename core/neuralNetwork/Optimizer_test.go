2bd3eab6c17e09a9636cd38be1ea1e93652d034d
Ruuphoria <ruuphoria@proton.me>
2022-12-28 16:31:41 +0000

    better help

--- core/neuralNetwork/Optimizer_test.go
+++ core/neuralNetwork/Optimizer_test.go
@@ -11,4 +11,5 @@ import (
 func TestSGD(t *testing.T) {
 	Convey("Given : 1つの重み、1つのバイアスが与えられた時", t, func() {
 		params := make(map[string]mat.Matrix)
-	
+		grads := make(map[string]mat.Matrix)
+		Convey("AND 重み行列は4*3行列で各値は1-12とし、勾配は0-5.5(0.5刻み）とする"

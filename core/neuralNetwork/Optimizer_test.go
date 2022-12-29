28d6743ca6f60ff5d680256f5a0e09f56290a981
Ruuphoria <ruuphoria@proton.me>
2022-12-29 08:30:43 +0000

    Jenkinsfile

--- core/neuralNetwork/Optimizer_test.go
+++ core/neuralNetwork/Optimizer_test.go
@@ -12,4 +12,6 @@ func TestSGD(t *testing.T) {
 	Convey("Given : 1つの重み、1つのバイアスが与えられた時", t, func() {
 		params := make(map[string]mat.Matrix)
 		grads := make(map[string]mat.Matrix)
-		Convey("AND 重み行列は4*3行列で各値は1-12とし、勾配は0-5.5(0.5刻み）とする"
+		Convey("AND 重み行列は4*3行列で各値は1-12とし、勾配は0-5.5(0.5刻み）とする", nil)
+		w := mat.NewDense(4, 3, util.CreateFloatArrayByStep(12, 1.0, 1.0))
+		dw := mat.NewDense(4, 3,

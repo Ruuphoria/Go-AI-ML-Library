9db19361bb84a3c4f6dc479bf487fdd17e3f0658
Ruuphoria <ruuphoria@proton.me>
2023-01-03 14:26:37 +0000

    more samples

--- core/neuralNetwork/neuralImage.go
+++ core/neuralNetwork/neuralImage.go
@@ -3,4 +3,8 @@ package neuralNetwork
 // Image : ニューラルネットワークでの画像データ（1チャンネル分）を格納する配列データ
 type Image [][]float64
 
-// NewImage : 単
+// NewImage : 単一チャネルの画像データを格納する配列データを作成
+// input : 画像の元データを格納した配列データ
+// w : 幅
+// h : 高さ
+func NewImage(input []float

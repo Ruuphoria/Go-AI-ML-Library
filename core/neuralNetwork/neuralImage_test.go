b1676d4dcd8a2ccc098adf17342093c2b0697c7f
Ruuphoria <ruuphoria@proton.me>
2023-01-11 21:02:25 +0000

    Imported folder structure

--- core/neuralNetwork/neuralImage_test.go
+++ core/neuralNetwork/neuralImage_test.go
@@ -12,4 +12,7 @@ func TestNewImage(t *testing.T) {
 	Convey("Given : 3 * 4の数値が配列で与えられた時", t, func() {
 		w := 3
 		h := 4
-		input := util.Create
+		input := util.CreateFloatArrayByStep(w*h, 0, 1)
+		Convey("When : Imageを作成する", func() {
+			image := NewImage(input, w, h)
+			Conv

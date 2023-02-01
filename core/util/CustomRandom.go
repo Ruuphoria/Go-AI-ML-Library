2b05b52c92781f40af7045a33421f758e29b2a3f
Ruuphoria <ruuphoria@proton.me>
2023-02-01 05:29:47 +0000

    library

--- core/util/CustomRandom.go
+++ core/util/CustomRandom.go
@@ -5,4 +5,8 @@ import (
 	"time"
 )
 
-func randomFloat(r *rand.Ra
+func randomFloat(r *rand.Rand, min, max float64) float64 {
+	return r.Float64()*(max-min) + min
+}
+
+func randomFloat32(r *rand.Rand, min, ma

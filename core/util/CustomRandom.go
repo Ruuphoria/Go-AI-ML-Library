d901e83af1bbc597d64988b4bd27b966b661a40e
Ruuphoria <ruuphoria@proton.me>
2023-02-02 15:43:05 +0000

    Update REAMDE.md

--- core/util/CustomRandom.go
+++ core/util/CustomRandom.go
@@ -9,4 +9,8 @@ func randomFloat(r *rand.Rand, min, max float64) float64 {
 	return r.Float64()*(max-min) + min
 }
 
-func randomFloat32(r *rand.Rand, min, ma
+func randomFloat32(r *rand.Rand, min, max float32) float32 {
+	return r.Float32()*(max-min) + min
+}
+
+func RandomFloatArray(min, max fl

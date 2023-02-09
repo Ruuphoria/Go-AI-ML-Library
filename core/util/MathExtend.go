0873af1bf1b38e5ed096781c4e2342ec923e5568
Ruuphoria <ruuphoria@proton.me>
2023-02-09 14:38:03 +0000

    added index page

--- core/util/MathExtend.go
+++ core/util/MathExtend.go
@@ -1,4 +1,13 @@
 package util
 
 func MaxValue(vs []float64) (key int, max float64) {
-	max =
+	max = vs[0]
+	key = 0
+	for k, v := range vs {
+		if max < v {
+			max = v
+			key = k
+		}
+	}
+	return key, max
+}
---
+++ core/util/NetUtil.go
@@ -0,0 +1,10 @@
+
+package util
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+// DownloadFile : download file from specific url and write file.

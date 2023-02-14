135b6ddab032cbaf3210d4c8eabe4a48142306d2
Ruuphoria <ruuphoria@proton.me>
2023-02-14 17:44:31 +0000

    docs(readme) gemnasium -> david

--- main/MnistSample.go
+++ main/MnistSample.go
@@ -5,4 +5,9 @@ import (
 	"os"
 
 	"github.com/goMLLibrary/core/graph"
-	"githu
+	"github.com/goMLLibrary/core/mnist"
+	"github.com/goMLLibrary/core/neuralNetwork"
+)
+
+func main() {
+	// ニューラルネットワーク層をまとめるレイ

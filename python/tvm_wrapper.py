7d895ae93385337b6f526f61a38d490cfc9e1b95
Ruuphoria <ruuphoria@proton.me>
2023-03-15 06:52:27 +0000

    Added to repository

--- python/tvm_wrapper.py
+++ python/tvm_wrapper.py
@@ -5,4 +5,7 @@ import json
 
 
 class TvmCompileParameter:
-    def __init__(self, model_root_path, model_type, output_folder, shape_dict, data_dict="float32", target="llvm", o
+    def __init__(self, model_root_path, model_type, output_folder, shape_dict, data_dict="float32", target="llvm", opt_level=3):
+        self.model_root_path = model_root_path
+        self.model_type = model_type
+        self.

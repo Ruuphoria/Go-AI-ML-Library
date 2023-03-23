d239d7ca40498114b6168f15370838f4b065813e
Ruuphoria <ruuphoria@proton.me>
2023-03-23 15:26:12 +0000

    update cs.po

--- python/tvm_wrapper.py
+++ python/tvm_wrapper.py
@@ -8,4 +8,7 @@ class TvmCompileParameter:
     def __init__(self, model_root_path, model_type, output_folder, shape_dict, data_dict="float32", target="llvm", opt_level=3):
         self.model_root_path = model_root_path
         self.model_type = model_type
-        self.
+        self.output_folder = output_folder
+        self.target = target
+        self.opt_level = opt_level
+        se
